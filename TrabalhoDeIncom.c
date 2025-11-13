c7ca20446e96181316953c79e25837f3a646279f
outz1 <du.ebrits@gmail.com>
2025-11-13 13:06:01 -0300

    inicializando a função main que mostra o menu da agenda

--- TrabalhoDeIncom.c
+++ TrabalhoDeIncom.c
@@ -32,3 +32,56 @@ void editar_compromisso();
 void mudar_prioridade();
 int verificar_disponibilidade(int dia, int hora, int duracao);
 void limpar_buffer();
+
+
+int main() {
+    int opcao;
+    
+    printf("\n");
+    printf("══════════════════════════════\n");
+    printf("        AGENDA SEMANAL         \n");
+    printf("══════════════════════════════\n");
+    
+    // Inicializa tudo zerado
+    inicializar_agenda();
+    
+    // Loop principal
+    do {
+        mostrar_menu();
+        printf("\nEscolha uma opção: ");
+        
+        if(scanf("%d", &opcao) != 1) {
+            printf("Opção inválida! Digite um número.\n");
+            limpar_buffer();
+            continue;
+        }
+        limpar_buffer();
+        
+        // Switch pra ficar mais organizado
+        switch(opcao) {
+            case 1:
+                adicionar_compromisso();
+                break;
+            case 2:
+                remover_compromisso();
+                break;
+            case 3:
+                ver_agenda();
+                break;
+            case 4:
+                editar_compromisso();
+                break;
+            case 5:
+                mudar_prioridade();
+                break;
+            case 0:
+                printf("\nAté mais! Saindo da agenda...\n");
+                break;
+            default:
+                printf("Opção não existe! Tente de novo.\n");
+        }
+        
+    } while(opcao != 0);
+    
+    return 0;
+}
