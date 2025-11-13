2cf4fec8f4a161d3e359a4624bbab54811a3b071
outz1 <du.ebrits@gmail.com>
2025-11-13 13:05:35 -0300

    definição da agenda em dias e horas e inicializando o nome de todas as funções essenciais

--- TrabalhoDeIncom.c
+++ TrabalhoDeIncom.c
@@ -14,3 +14,21 @@ struct Compromisso {
     int duracao;        // em horas
     int prioridade;     // 0 = normal, 1 = alta
 };
+
+
+// Agenda global - matriz de dias x horas
+struct Compromisso agenda[DIAS][HORAS];
+
+// Nomes dos dias pra mostrar bonitinho
+char *nomes_dias[] = {"Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"};
+
+// Protótipos das funções
+void inicializar_agenda();
+void mostrar_menu();
+void adicionar_compromisso();
+void remover_compromisso();
+void ver_agenda();
+void editar_compromisso();
+void mudar_prioridade();
+int verificar_disponibilidade(int dia, int hora, int duracao);
+void limpar_buffer();
